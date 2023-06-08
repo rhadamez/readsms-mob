34086728cdfdf339020488b10d3f8f64e8a97498
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-06-08 01:42:27 -0300

    feat: first commit

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useRef, useState } from 'react';
+import { View, TextInput } from 'react-native';
+
+import { styles } from './styles'
+
+export default function App() {
+  const [totalInputs] = useState(4)
+  const inputRef = useRef<TextInput[]>([])
+
+  function handleTextChange(text: string, index: number) {
+    if(text === '') return
+    else if(text.length === 1) {
+      inputRef.current[index].setNativeProps({ text: text.charAt(0) })
+      if(index < totalInputs) inputRef.current[index+1].focus()
+      else inputRef.current[index].blur()
+    }
+    else if(index <= totalInputs) {
+      let nextIndex = index
+      let currentIsFilled = false
+      for(let i = 0; i <= text.length; i++) {
+        const currentChar = text.charAt(i)
+        if(!currentIsFilled) {
+          inputRef.current[index].setNativeProps({ text: currentChar })
+          if(nextIndex <= totalInputs) inputRef.current[nextIndex].focus()
+          currentIsFilled = true
+        } else if(nextIndex <= totalInputs) {
+          inputRef.current[nextIndex].setNativeProps({ text: currentChar })
+          if(nextIndex+1 <= totalInputs) inputRef.current[nextIndex+1].focus()
+        }
+        nextIndex++
+      }
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style='dark' />
+      {Array.from({ length: totalInputs }, (_, index) => index+1).map(item => (
+        <TextInput
+          ref={(ref) => { inputRef.current[item] = ref }}
+          onChange={(text) => handleTextChange(text.nativeEvent.text, item)}
+          key={item}
+          style={styles.input} />
+      ))}
+    </View>
+  );
+}
---
+++ styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#121214'
+  },
+  input: {
+    height: 50,
+    width: 50,
+    borderRadius: 5,
+    backgroundColor: '#8257e5',
+    padding: 5,
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20
+  }
+})
